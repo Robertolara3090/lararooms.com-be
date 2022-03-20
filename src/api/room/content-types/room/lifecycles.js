b01aaccdc2e29c58bcf992d85a12604f67f4082d
Ruben Costa <daggosso2@gmail.com>
2022-03-20 20:53:48 +0000

    update url

--- src/api/room/content-types/room/lifecycles.js
+++ src/api/room/content-types/room/lifecycles.js
@@ -3,17 +3,17 @@ const { default: axios } = require("axios");
 module.exports = {
   afterCreate(event) {
     axios.get(
-      `https://lararooms-com-fe.vercel.app/api/revalidate?secret=${process.env.MY_SECRET_TOKEN}`
+      `https://lararooms.com/api/revalidate?secret=${process.env.MY_SECRET_TOKEN}`
     );
   },
   afterUpdate(event) {
     axios.get(
-      `https://lararooms-com-fe.vercel.app/api/revalidate?secret=${process.env.MY_SECRET_TOKEN}`
+      `https://lararooms.com/api/revalidate?secret=${process.env.MY_SECRET_TOKEN}`
     );
   },
   afterDelete(event) {
     axios.get(
-      `https://lararooms-com-fe.vercel.app/api/revalidate?secret=${process.env.MY_SECRET_TOKEN}`
+      `https://lararooms.com/api/revalidate?secret=${process.env.MY_SECRET_TOKEN}`
     );
   },
 };
